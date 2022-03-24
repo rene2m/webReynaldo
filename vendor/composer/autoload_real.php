b18ff1fc2252d9605f1b3705d4f9ad6356e47788
rene2m <rene.mamani.sto@gmail.com>
2022-03-24 23:18:05 +0100

    deploy heroku

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,16 +65,11 @@ class ComposerAutoloaderInit15c08a94f3f109eeb9771db661827f84
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequire15c08a94f3f109eeb9771db661827f84($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
